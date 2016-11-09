0d2b4a1de336de1339128e3b6a87f4db67a79735
Andrea Lovato <lovaex@gmail.com>
2016-11-09 11:52:51 +0100

    reordered solution

--- SolrNet.Tests.Integration/IntegrationFixture.cs
+++ SolrNet.Tests.Integration/IntegrationFixture.cs
@@ -21,7 +21,6 @@ using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
-using MbUnit.Framework;
 using Microsoft.Practices.ServiceLocation;
 using NUnit.Framework;
 using SolrNet.Commands.Parameters;
